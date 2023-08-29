e072f8539c0a9587686d91731f12735a1c413eb8
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-29 16:30:28 +0700

    refactor code

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
-import rootReducer from "./rootReducer";
-import rootSaga from "./rootSaga";
+import rootReducer from "./reducer";
+import rootSaga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, compose(applyMiddleware(sagaMiddleware)));
